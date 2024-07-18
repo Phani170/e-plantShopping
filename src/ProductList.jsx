11f482f769cec145c39966b1a985c1f1944fdc0d
Phani170 <71382398+Phani170@users.noreply.github.com>
2024-07-18 15:30:20 -0400

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,4 +1,5 @@
 import React, { useState,useEffect } from 'react';
+import { addItem } from './CreatSlice';
 import './ProductList.css'
 function ProductList() {
     const plantsArray = [
@@ -208,6 +209,17 @@ function ProductList() {
             ]
         }
     ];
+
+   const [addedToCart, setAddedToCart] = useState({});
+
+   const handleAddToCart = (product) => {
+    dispatch(addItem(product));
+    setAddedToCart((prevState) => ({
+       ...prevState,
+       [product.name]: true, // Set the product name as key and value as true to indicate it's added to cart
+     }));
+  };
+
    const styleObj={
     backgroundColor: '#4CAF50',
     color: '#fff!important',
@@ -251,7 +263,23 @@ function ProductList() {
         </div>
 
         <div className="product-grid">
+        {plantsArray.map((category, index) => (
+  <div key={index}>
+    <h1><div>{category.category}</div></h1>
+    <div className="product-list">
+      {category.plants.map((plant, plantIndex) => (
+        <div className="product-card" key={plantIndex}>
+          <img className="product-image" src={plant.image} alt={plant.name} />
+         <div className="product-title">{plant.name}</div>
+         <div>{plant.description}</div>
+         <div className='product-price'>{plant.cost}</div>
 
+         <button  onClick={() => handleAddToCart(plant)}>Add to Cart</button>
+        </div>
+      ))}
+    </div>
+  </div>
+))}
 
         </div>
 
