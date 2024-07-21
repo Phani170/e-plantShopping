90ad99145efff318353c1de4782adc877796de75
Phani170 <71382398+Phani170@users.noreply.github.com>
2024-07-21 01:03:43 -0400

    Update CartItem.jsx

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,27 +9,44 @@ const Cart = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+     let total = 0;
+    cart.forEach((item) => {
+        total += item.cost.replace('$','') * item.quantity;
+        console.log(total)
+    });
+    return total;
   };
 
+   const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+  };
+
+
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping();
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1){
+        dispatch(updateQuantity({name: item.name , quantity: item.quantity - 1}));
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    let tC;
+    tC = item.quantity * item.cost.replace('$','');
+    return tC;
   };
 
   return (
